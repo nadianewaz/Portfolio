00dcd66c4f19987ae3e23f8e284e2348d5dac206
nadianewaz <aninditaahmed@proton.me>
2023-07-13 12:48:53 -0400

    Adding image border

--- src/Component/HomePage/Banner/Banner.jsx
+++ src/Component/HomePage/Banner/Banner.jsx
@@ -37,7 +37,7 @@ const Banner = () => {
             width: 300,
             height: '300px',
             borderRadius: '50%',
-            border: 1
+            border: "10px solid black"
           }}/>
             </Typography>
            
