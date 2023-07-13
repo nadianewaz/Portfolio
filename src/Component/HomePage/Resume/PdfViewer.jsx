3244eb56656b090da94d4c348a7788981ce2bd9f
nadianewaz <aninditaahmed@proton.me>
2023-07-13 09:52:19 -0400

    Adding pdf view capability

---
+++ src/Component/HomePage/Resume/PdfViewer.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs  } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
+const PdfViewer = () => {
+    const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document file="Nazmus_Sakib_Resume.pdf" onLoadSuccess={onDocumentLoadSuccess} onLoadError={console.error}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+};
+
+export default PdfViewer;
