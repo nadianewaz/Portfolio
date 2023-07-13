661623a654217b86a8df509b50b6bc6a198d1130
nadianewaz <aninditaahmed@proton.me>
2023-07-13 00:43:45 -0400

    Adding project issue fix

--- src/Component/HomePage/Projects/Projects.jsx
+++ src/Component/HomePage/Projects/Projects.jsx
@@ -7,28 +7,26 @@ const projects = [
   {
     id: 1,
     name: ' Jewelry Management System',
-    description: 'Develop a desktop application that helps managing a
-    jewelry store. Store manager is able to add, buy or sell items. They can also search for items, see item details and see all the items in the inventory. This project is developed using Java and SQLite.',
+    description: 'Develop a desktop application that helps managing a jewelry store. Store manager is able to add, buy or sell items. They can also search for items, see item details and see all the items in the inventory. This project is developed using Java and SQLite.',
   
    },
   {
     id: 2,
     name: 'Doctors Portal',
-    description: 'Develop a doctor and patient portal with Register and Login
-    capabilities. Patients will be able to schedule appointments from the portal. This project is powered by React and Firebase.',
+    description: 'Develop a doctor and patient portal with Register and Login capabilities. Patients will be able to schedule appointments from the portal. This project is powered by React and Firebase.',
   
   },
   {
     id: 3,
     name: ' PSD to HTML conversion',
-    description: ' Built responsive and interactive web project. Used HTML5,
-    CSS3, Bootstrap, JavaScript, jQuery.',
+    description: ' Built responsive and interactive web project. Used HTML5, CSS3, Bootstrap, JavaScript, jQuery.',
   
   }
   
 ];
 
 const Projects = () => {
+  
     return (
         <>
         <Typography sx={{textTransform: 'capitalize', color: '#615F8B', m: 4}}>
