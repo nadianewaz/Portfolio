ab43e0c0c6ed564bec9a48d9003499b44516d85d
Nitu <aninditaahmed@proton.me>
2023-07-24 09:02:42 -0400

    Adding link to project name

--- src/Component/HomePage/Project/Project.jsx
+++ src/Component/HomePage/Project/Project.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Grid, Paper } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 
 
 
 
 const Project = ({project}) => {
-    console.log(project);
     const { name, description, link } = project; 
 
     console.log(name);
@@ -16,15 +16,12 @@ const Project = ({project}) => {
 <Grid item xs={12} sm={6} md={4}>
         <Paper elevation={3} sx={{height: "220px", py: 5,px: 3}} >
         <Typography sx={{fontWeight: '400', color: '#615F8B'}} variant="h5" gutterBottom>
-           {name} 
+        <Link to={link} target="_blank">{name} </Link>
         </Typography>
         <Typography sx={{textAlign: "right"}} variant="subtitle3" gutterBottom>
            {description} 
         </Typography>
         <br />
-        <Typography sx={{fontWeight: "bold", textAlign: "left"}} variant="subtitle3" gutterBottom>
-           {link} 
-        </Typography>
        
     
 
