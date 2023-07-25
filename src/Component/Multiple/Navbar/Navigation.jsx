368c357fe8a571cd5c17f8147ebf4fa79db334a4
Nitu <aninditaahmed@proton.me>
2023-07-25 08:59:01 -0400

    adding all files related to scrolling home page

--- src/Component/Multiple/Navbar/Navigation.jsx
+++ src/Component/Multiple/Navbar/Navigation.jsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 
 const Navigation = () => {
     return (
@@ -12,26 +12,26 @@ const Navigation = () => {
                                      
 
 <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{backgroundColor: '#615F8B'}}>
+      <AppBar position="fixed" sx={{backgroundColor: '#615F8B'}}>
       <Toolbar>
           
 
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left', fontSize: '28px' }}>
             Nadia Tamanna Nitu  
           </Typography>
-          <Link to="/">
+          <Link to="home" spy={true} smooth={true} offset={-70} duration={500}>
           <Button sx={{color: '#fff'}}>Home   </Button>
           </Link>
-          <Link to="/about">
+          <Link to="about" spy={true} smooth={true} offset={-70} duration={500}>
           <Button sx={{color: '#fff'}}>About  </Button>
           </Link>
-          <Link to="/toolkit">
+          <Link to="toolkit" spy={true} smooth={true} offset={-70} duration={500}>
           <Button sx={{color: '#fff'}}>Toolkit </Button>
           </Link>
-          <Link to="/projects">
+          <Link to="projects" spy={true} smooth={true} offset={-70} duration={500}>
           <Button sx={{color: '#fff'}}>Projects</Button>
           </Link>
-          <Link to="/resume">
+          <Link to="resume" spy={true} smooth={true} offset={-70} duration={500}>
           <Button sx={{color: '#fff'}}>Resume </Button>
           </Link>
          
