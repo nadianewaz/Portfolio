312a9643b59863d3608ac2e63a822689852a35ad
Nitu <aninditaahmed@proton.me>
2023-07-30 21:10:54 -0400

    Adding pdf alignment styling

--- src/Component/HomePage/Resume/PdfViewer.jsx
+++ src/Component/HomePage/Resume/PdfViewer.jsx
@@ -4,23 +4,40 @@ import { Document, Page, pdfjs  } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 const PdfViewer = () => {
-    const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(null);
+	const [pageNumber, setPageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+	const onDocumentLoadSuccess = ({ numPages }) => {
+		setNumPages(numPages);
+	};
 
-  return (
-    <div>
-      <Document file="Resume-of-Nadia-Tamanna-Nitu.pdf" onLoadSuccess={onDocumentLoadSuccess} onLoadError={console.error}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-    </div>
-  );
+	const goToPrevPage = () =>
+		setPageNumber(pageNumber - 1 <= 1 ? 1 : pageNumber - 1);
+
+	const goToNextPage = () =>
+		setPageNumber(
+			pageNumber + 1 >= numPages ? numPages : pageNumber + 1,
+		);
+
+	return (
+		<div>
+			<nav>
+				<button onClick={goToPrevPage}>Prev</button>
+				<button onClick={goToNextPage}>Next</button>
+				<p>
+					Page {pageNumber} of {numPages}
+				</p>
+			</nav>
+
+			<Document
+				file="Resume-of-Nadia-Tamanna-Nitu.pdf"
+				onLoadSuccess={onDocumentLoadSuccess}
+			>
+				<Page pageNumber={pageNumber} />
+			</Document>
+		</div>
+	);
+
 };
 
 export default PdfViewer;
